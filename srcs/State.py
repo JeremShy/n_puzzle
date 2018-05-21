c22e12fcf7af50f48cf3bf2f666c3e29b96e67c5
jcamhi <jeremshy@h3lp.fr>
2018-05-21 19:55:02 +0200

    Ok mais fause solution

--- srcs/State.py
+++ srcs/State.py
@@ -1,11 +1,16 @@
 from random import shuffle
 import re
+import functools
+from copy import deepcopy
 #from srcs.globals import g_hash_end_state
 # from srcs.globals import g_hash_end_state
 
 import srcs.globals
 
 class State:
+	def __lt__(self, other):
+		return (0)
+
 	def __generateRandom(self, size):
 		listeuh = [i for i in range(size * size)]
 		shuffle(listeuh)
@@ -17,7 +22,7 @@ class State:
 			self.state.append(tmp)
 
 	def __parseFirstLine(self, listeuh):
-		print("In first line")
+		# print("In first line")
 		if len(listeuh) != 1:
 			raise NPuzzleError("Invalid first line")
 		number = int(listeuh[0])
@@ -26,15 +31,15 @@ class State:
 		return (number)
 	
 	def __parseOtherLine(self, listeuh):
-		print("In other line")
+		# print("In other line")
 		if len(listeuh) != self.size:
 			raise NPuzzleError("Invalid line in file")
 		temp = [int(i) for i in listeuh]
 		self.state.append(temp)
-		print (temp)
+		# print (temp)
 
 	def __parseFile(self, filename):
-		print ("Parsing file : " + filename)
+		# print ("Parsing file : " + filename)
 		firstTime = True
 		file = open(filename, "r")
 		for line in file:
@@ -45,7 +50,7 @@ class State:
 			if not handled:
 				continue
 			temp = handled.split()
-			print (temp)
+			# print (temp)
 			try:
 				if firstTime == True:
 					self.size = self.__parseFirstLine(temp)
@@ -80,27 +85,137 @@ class State:
 
 	def manhattanDistance(self):
 		global g_hash_end_state
-		print (srcs.globals.g_hash_end_state)
+		# print (srcs.globals.g_hash_end_state)
 		acc = 0
 		for i in range (self.size * self.size):
 			 acc += abs(i % self.size - srcs.globals.g_hash_end_state[self.state[i % self.size][i // self.size]][0]) + abs(i // self.size - srcs.globals.g_hash_end_state[self.state[i % self.size][i // self.size]][1])
 		return acc
 
+	def calcHeuristique(self):
+		global g_hash_end_state
+		if (srcs.globals.g_hash_end_state != []):
+			self.heuristique = self.manhattanDistance()
+	
+	def canMoveUp(self):
+		for y in range(self.size):
+			for x in range(self.size):
+				if (self.state[y][x] == 0):
+					if (y == 0):
+						return False
+					else:
+						return True
+	
+	def getMovedUp(self):
+		copy = State(state=self)
+		for y in range(copy.size):
+			for x in range(copy.size):
+				if (copy.state[y][x] == 0):
+					tmp = copy.state[y - 1][x]
+					copy.state[y - 1][x] = copy.state[y][x]
+					copy.state[y][x] = tmp
+					copy.calcHeuristique()
+					return copy
+
+	def canMoveDown(self):
+		for y in range(self.size):
+			for x in range(self.size):
+				if (self.state[y][x] == 0):
+					if (y == self.size - 1):
+						return False
+					else:
+						return True
+	
+	def getMovedDown(self):
+		copy = State(state=self)
+		for y in range(copy.size):
+			for x in range(copy.size):
+				if (copy.state[y][x] == 0):
+					tmp = copy.state[y + 1][x]
+					copy.state[y + 1][x] = copy.state[y][x]
+					copy.state[y][x] = tmp
+					copy.calcHeuristique()
+					return copy
+
+	def canMoveLeft(self):
+		for y in range(self.size):
+			for x in range(self.size):
+				if (self.state[y][x] == 0):
+					if (x == 0):
+						return False
+					else:
+						return True
+	
+	def getMovedLeft(self):
+		copy = State(state=self)
+		for y in range(copy.size):
+			for x in range(copy.size):
+				if (copy.state[y][x] == 0):
+					tmp = copy.state[y][x - 1]
+					copy.state[y][x - 1] = copy.state[y][x]
+					copy.state[y][x] = tmp
+					copy.calcHeuristique()
+					return copy
+
+	def canMoveRight(self):
+		for y in range(self.size):
+			for x in range(self.size):
+				if (self.state[y][x] == 0):
+					if (x == self.size - 1):
+						return False
+					else:
+						return True
+	
+	def getMovedRight(self):
+		copy = State(state=self)
+		# print ("Init")
+		# print(copy)
+		for y in range(copy.size):
+			for x in range(copy.size):
+				if (copy.state[y][x] == 0):
+					tmp = copy.state[y][x + 1]
+					copy.state[y][x + 1] = copy.state[y][x]
+					copy.state[y][x] = tmp
+					copy.calcHeuristique()
+					# print ("copy")
+					# print (copy)
+					return copy
+
+	def getNeighbors(self):
+		ret = []
+		if (self.canMoveUp() == True):
+			ret.append(self.getMovedUp())
+		if (self.canMoveDown() == True):
+			ret.append(self.getMovedDown())
+		if (self.canMoveLeft() == True):
+			ret.append(self.getMovedLeft())
+		if (self.canMoveRight() == True):
+			ret.append(self.getMovedRight())
+		return (ret)
+
 	def __init__(self, *args, **kwargs):
 		self.state = []
 		if ("file" in kwargs):
-			print ("We need to parse the file " + kwargs["file"])
+			# print ("We need to parse the file " + kwargs["file"])
 			self.__parseFile(kwargs["file"])
 			if self.__isValid() == False:
 				raise NPuzzleError("Invalid map.")
 		elif ("size" in kwargs):
-			print ("We need to generate a NxN puzzle with N=" + str(kwargs["size"]))
+			# print ("We need to generate a NxN puzzle with N=" + str(kwargs["size"]))
 			if (kwargs["size"] < 3):
 				raise NPuzzleError("Can't generate a puzzle with size lower than 3.")
 			self.__generateRandom(kwargs["size"])
 			self.size = int(kwargs["size"])
 		elif ("liste" in kwargs):
-			self.state = kwargs["liste"]
+			self.state = deepcopy(kwargs["liste"])
 			self.size = len(kwargs["liste"])
+		elif ("state" in kwargs):
+			self.state = deepcopy(kwargs["state"].state)
+			self.size = kwargs["state"].size
+			self.heuristique = kwargs["state"].heuristique
+			self.g = kwargs["state"].g + 1
+			return ;
 		else:
 			raise NPuzzleError("You must specify a 'file' or 'size' argument.")
+		self.calcHeuristique()
+		self.g = 0
+
--- srcs/algo.py
+++ srcs/algo.py
@@ -1,14 +1,75 @@
 # from srcs.globals import srcs.globals.g_hash_end_state
 import srcs.globals
+from queue import PriorityQueue
+from queue import Queue
 
 def	initialize_map(end_state):
-	print (srcs.globals.g_hash_end_state)
+	# print (srcs.globals.g_hash_end_state)
 	srcs.globals.g_hash_end_state = [1 for i in range (end_state.size * end_state.size)]
 	for i in range(end_state.size):
 		for j in range(end_state.size):
 			srcs.globals.g_hash_end_state[end_state.state[i][j]] = (i, j)
-	print (srcs.globals.g_hash_end_state)
+	# print (srcs.globals.g_hash_end_state)
+
+def get_lowest_f(liste):
+	min_val = -1
+	for elem in liste:
+		if (min_val == -1 or elem.g + elem.heuristique <= min_val):
+			min_val = elem.g + elem.heuristique
+			min_elem = elem
+	liste.remove(min_elem)
+	# print ("min : " + str(min_val))
+	return min_elem
+
+def countain(liste, to_check):
+	for elem in liste:
+		if elem.state == to_check.state:
+			return elem
+	return False
+
+def countain_pq(liste, to_check):
+	for heuristique, elem in liste.queue:
+		if elem.state == to_check.state:
+			return elem
+	return False
+
 
 def solve(initial_state, end_state):
 	initialize_map(end_state)
-	print (initial_state.manhattanDistance())
+	initial_state.calcHeuristique()
+	end_state.calcHeuristique()
+	# print (initial_state)
+	# openset = []
+	openset = PriorityQueue()
+
+	openset.put_nowait((initial_state.heuristique, initial_state))
+	closedset = []
+
+	# print("Initial state")
+	# print (initial_state)
+	while (openset):
+		current_heur, current = openset.get_nowait()
+		closedset.append(current)
+		# print (current)
+		
+		if (current.heuristique == 0):
+			print("Is ok")
+			print (current)
+			print (current.g)
+			return True
+		neighbors = current.getNeighbors()
+		if (not neighbors):			
+			continue
+		for neighbor in neighbors:
+			if countain(closedset, neighbor):
+				continue
+			tmp = countain_pq(openset, neighbor)
+			if (tmp == False):
+				pouet = (neighbor.heuristique, neighbor)
+				openset.put_nowait(pouet)
+			else:
+				if (tmp.g > neighbor.g):
+					# print ("replacing " + str(tmp.heuristique + tmp.g) + "with" + str(neighbor.heuristique + neighbor.g))
+					tmp.g = neighbor.g
+					# TODO predecesseur
+
